70606d3138347f8fb452bc3e29726d6a38ed3553
n16h7c0r3 <57857338+n16h7c0r3@users.noreply.github.com>
2020-04-22 14:11:32 +0300

    Add files via upload

---
+++ register_login/app.py
@@ -0,0 +1,48 @@
+import base64
+import os
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+x=0
+while x==0:
+    login_prov = input('create login: ')
+    login = login_prov.encode()
+    
+    check = open('names.db', 'r')
+    lines = check.readlines()
+    check.close()
+    for line in lines:
+        if line == login_prov + '\n':
+            print('user allready exists')
+            x=0
+        else:
+            x=1
+
+password_prov = input('create password: ')
+password = password_prov.encode()
+
+salt = b'\x13\xab3\x7f}\xae\xf0\x8aiN\x94\xec\xe1\x8c,j'
+kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=100000,
+    backend=default_backend()
+)
+
+key = base64.urlsafe_b64encode(kdf.derive(password))
+
+f = Fernet(key)
+
+hash_login = f.encrypt(login)
+hash_login_dec = hash_login.decode()
+
+fl = open('database.db', 'a')
+fl.write(hash_login_dec + '\n')
+fl.close()
+
+fl_name = open('names.db', 'a')
+fl_name.write(login_prov + '\n')
+fl_name.close()
---
+++ register_login/app_login.py
@@ -0,0 +1,63 @@
+import base64
+import sys
+import os
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+login_prov = input('login: ')
+login = login_prov.encode()
+
+db_num = open('names.db', 'r')
+lines = db_num.readlines()
+
+db_pass = open('database.db', 'r')
+lines_db = db_pass.readlines()
+
+line_num = 0
+
+for line in lines:
+        if line != login_prov + '\n':
+            line_num = line_num + 1
+        elif line == login_prov + '\n':
+            line_num = line_num + 1
+            login_prov_line = line_num
+
+line_checked = 0
+try:
+    for line in lines_db:
+        line_checked = line_checked + 1
+        if line_checked == login_prov_line:
+            hash_ = line
+except:
+    print('user doesn\'t exists')
+    input('press enter to exit')
+    sys.exit()
+
+hash_enc = hash_.encode()
+
+password_prov = input('password: ')
+password = password_prov.encode()
+
+salt = b'\x13\xab3\x7f}\xae\xf0\x8aiN\x94\xec\xe1\x8c,j'
+kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=100000,
+    backend=default_backend()
+)
+
+key = base64.urlsafe_b64encode(kdf.derive(password))
+
+f = Fernet(key)
+
+try:
+    hash_login = f.decrypt(hash_enc)
+    hash_login_dec = hash_login.decode()
+    print('Success!')
+except:
+    print('Wrong Password!')
+    input('press enter to exit')
+    sys.exit()
